d00232772821e8da465bdae831c7cc6a44b6213e
Lasitha Lankajeewa <lasithalankajeewa@gmail.com>
2023-11-30 17:10:48 +0530

    init

--- Scanner Test BL/MainPage.xaml.cs
+++ Scanner Test BL/MainPage.xaml.cs
@@ -1,9 +1,25 @@
-﻿namespace Scanner_Test_BL;
+﻿using Microsoft.Maui.Controls;
+
+namespace Scanner_Test_BL;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+        MyWebView.Navigating += WebView_Navigating;
+        MyWebView.Navigated += WebView_Navigated;
+    }
+
+    private void WebView_Navigating(object sender, WebNavigatingEventArgs e)
+    {
+        // Handle navigation event (before navigation)
+        // You can log or take any other action here
+    }
+
+    private void WebView_Navigated(object sender, WebNavigatedEventArgs e)
+    {
+        // Handle navigation event (after navigation)
+        // You can log or take any other action here
+    }
 }
